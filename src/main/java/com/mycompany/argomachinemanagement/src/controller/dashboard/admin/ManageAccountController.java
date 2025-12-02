e2e8d584f0c1c88c58e8c0f3f6f5fe72d7473347
{maitt03} <{tranthuymai334@gmail.com}>
2025-12-02 09:17:10 +0700

    base source Java Servlet

---
+++ src/main/java/com/mycompany/argomachinemanagement/src/controller/dashboard/admin/ManageAccountController.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.mycompany.argomachinemanagement.src.controller.dashboard.admin;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+
+@WebServlet(name = "ManageAccountController", urlPatterns = { "/manage-account" })
+public class ManageAccountController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null || action.equals("list")) {
+            handleListWithFilters(request, response);
+        } else {
+            switch (action) {
+                case "edit":
+                    showEditForm(request, response);
+                    break;
+                case "deactivate":
+                    deactivateAccount(request, response);
+                    break;
+            }
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "list"; // Default action
+        }
+
+        switch (action) {
+            case "update":
+                updateAccount(request, response);
+                break;
+            default:
+                handleListWithFilters(request, response);
+                break;
+        }
+    }
+
+    private void showEditForm(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+     
+        response.sendRedirect(request.getContextPath() + "/manage-account");
+    }
+
+    private void handleListWithFilters(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Get filter parameters
+       
+    }
+
+    private void updateAccount(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+    }
+
+    private void deactivateAccount(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+
+}
